c00f71a5dcc0f664e05078b882fedd2ee039913c
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-03-14 16:58:45 +0100

    cosmetics

--- src/server/index.js
+++ src/server/index.js
@@ -4,11 +4,15 @@ import 'source-map-support/register';
 import * as soundworks from 'soundworks/server';
 import SoundfieldExperience from './SoundfieldExperience';
 
+
 // sets the size of the area, orther setup informations are not needed
 const area = { height: 5, width: 8 };
+
 // initialize the server with configuration informations
 soundworks.server.init({ setup: { area }, appName: 'Soundfield' });
+
 // create the common server experience for both the soloists and the players
 const soundfieldExperience = new SoundfieldExperience(['player', 'soloist']);
+
 // start the application
 soundworks.server.start();
